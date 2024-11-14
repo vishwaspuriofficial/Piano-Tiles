c833f589e4225b6d4879aae7217d25d6217b4616
Ethan Lam <ethanchunkiu.lam@mail.utoronto.ca>
2024-11-13 19:47:15 -0500

    more ModelSim

--- ModelSim/testbench.tcl
+++ ModelSim/testbench.tcl
@@ -13,4 +13,4 @@ vsim work.testbench -Lf 220model -Lf altera_mf_ver -Lf verilog
 # show waveforms specified in wave.do
 do wave.do
 # advance the simulation the desired amount of time
-run 3000 ns
+run 50000 ns
